05e7df18cfd915e4fcb4ddf069655ab9654b8bc9
Nick Vegean <nick.vegean@foun.co>
2022-08-22 11:20:28 +0800

    component props withdefaults macro

--- src/components/StyledInput.vue
+++ src/components/StyledInput.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
-defineProps<{ modelValue: string, placeholder: string }>()
-defineEmits(['update:modelValue'])
+
+interface Props {
+    modelValue: string, placeholder?: string
+}
+withDefaults(defineProps<Props>(), {
+    placeholder: 'Enter Text'
+})
+const emit = defineEmits<{ (e: 'update:modelValue', event: string): void }>()
+
 </script>
 
 <template>
     <input :placeholder="placeholder" :value="modelValue"
-        @change="$emit('update:modelValue', ($event.target as HTMLInputElement).value)" placeholder="Name"
+        @change="emit('update:modelValue', ($event.target as HTMLInputElement).value)" placeholder="Name"
         class="block p-2.5 w-full text-sm text-gray-700 rounded-md border border-gray-300 focus:ring-blue-500 focus:border-blue-500" />
 </template>
